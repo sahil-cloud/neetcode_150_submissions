b4baf5e1166c7aed0dc7d499088462963c05b421
sahil jasuja <sahiljasuja666@gmail.com>
2026-04-03 17:41:31 +0530

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=1) return n;
+        sort(nums.begin(),nums.end());
+        int lcs = 1;
+        int pv = nums[0];
+        int curr = 1;
+        for(int i=1;i<n;i++){
+            if(nums[i] == pv+1){
+                
+                pv = nums[i];
+                curr += 1;
+            }else if(nums[i] == pv){
+                // cout<<pv<<endl;
+                continue;
+            }
+            else{
+                // cout<<pv<<" "<<curr<<endl;
+                lcs = max(curr,lcs);
+                curr=1;
+                pv = nums[i];
+            }
+        }
+        lcs = max(curr,lcs);
+        return lcs;
+    }
+};
