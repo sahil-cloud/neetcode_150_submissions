dcc79dd1ccd2918342910c465ccd50caf73fabd2
sahil jasuja <sahiljasuja666@gmail.com>
2026-07-19 23:51:01 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int me = nums[0],mec=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] != me) mec--;
+            else mec++;
+            if(mec == 0) {me = nums[i]; mec = 1;}
+        }
+        return me;
+    }
+};
