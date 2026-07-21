07fc892f6976035f3160cad7a1562d765d915a39
sahil jasuja <sahiljasuja666@gmail.com>
2026-07-21 14:26:47 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low = 0, high = x/2;
+        int ans = 1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(mid*mid == x) return mid;
+            else if(mid*mid < x){
+                ans = max(ans,mid);
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+};
